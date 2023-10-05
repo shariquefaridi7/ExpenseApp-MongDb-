77a39e5188512b48f40e08fe172304405583c8c2
shariquefaridi7 <shariquefaridi786@gmail.com>
2023-10-05 20:57:20 +0530

    FinalSubmit

--- backend/util/dbconfig.js
+++ backend/util/dbconfig.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const sequelize=new Sequelize(process.env.DB,process.env.USER,process.env.DBPASSWORD,{
+export const sequelize=new Sequelize(process.env.DB,process.env.USERDB,process.env.DBPASSWORD,{
     host:process.env.DBHOST,
     dialect:"mysql"
 })
